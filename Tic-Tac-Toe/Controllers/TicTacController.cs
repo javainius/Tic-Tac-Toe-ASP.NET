1afee2192b4c16efbde3604848e8fa2e211d9820
Vainius <Vainius@PC737>
2020-01-27 13:00:12 +0200

    first commit

---
+++ Tic-Tac-Toe/Controllers/TicTacController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Tic_Tac_Toe.Controllers
+{
+    public class TicTacController : Controller
+    {
+        [HttpPost]
+        public JsonResult UsersTurn([FromBody] string fieldId)
+        {
+            return Json("Success"); ;
+        }
+    }
+}
---
+++ Tic-Tac-Toe/wwwroot/js/JavaScript.js
@@ -0,0 +1,27 @@
+﻿var turn = 'x';
+$(".box").on("click", function () {
+    if ($(this).is(':empty') && turn === 'x') {
+        $(this).html('X').css({ backgroundColor: "#580817" });
+        turn = 'o';
+        var message = '';
+        var fieldId = this.id;
+        $.post({
+            type: "POST",
+            url: '/TicTac/UsersTurn',
+            data: JSON.stringify(fieldId),
+            contentType: "application/json",
+            dataType: "json",
+            success: function (msg) {
+                alert(msg);
+                message = msg;
+            }
+        });
+        if (message === 'Success') {
+
+        }
+    }
+    else if ($(this).is(':empty')){
+        $(this).html('O').css({ backgroundColor: "#a25a01" });
+        turn = 'x';
+    }
+});
